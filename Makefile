e64d4ceee416060358465c1b2e079349f47d6e8d
Michal Migurski <mike@teczno.com>
2018-02-10 10:06:02 -0800

    Added ACS pop & demographics for census tracts to output GeoJSON

--- Makefile
+++ Makefile
@@ -3,8 +3,8 @@ all: PA-Precinct-Model.US-House-2016.votes.geojson \
      PA-Precinct-Model.PA-Senate-2016.votes.geojson
 
 # Spatial votes are calculated from raw votes and geographic areas
-%.geojson: %.csv.gz
-	./merge-layers.py PA-Geographies.gpkg $< $@
+%.geojson: ACS-data.csv %.csv.gz
+	./merge-layers.py PA-Geographies.gpkg $^ $@
 
 # Raw votes are calculated from Dem proportion and turnout estimates
 %.votes.csv.gz: %.propD.csv.gz %.turnout.csv.gz
--- merge-layers.py
+++ merge-layers.py
@@ -5,18 +5,32 @@ from osgeo import ogr
 logging.basicConfig(stream=sys.stderr, level=logging.INFO, format='%(levelname)09s - %(message)s')
 
 parser = argparse.ArgumentParser(description='Merge layers and votes')
-parser.add_argument('filename1', help='Spatial file with layers and areas')
-parser.add_argument('filename2', help='Tabular CSV file with vote counts')
-parser.add_argument('filename3', help='Output GeoJSON file with areas and votes')
+parser.add_argument('geo_name', help='Spatial file with layers and areas')
+parser.add_argument('acs_name', help='Tabular CSV file with ACS population')
+parser.add_argument('votecsv_name', help='Tabular CSV file with vote counts')
+parser.add_argument('out_name', help='Output GeoJSON file with areas and votes')
 
 args = parser.parse_args()
+
+logging.info('Reading ACS population from {}...'.format(args.acs_name))
+
+populations = collections.defaultdict(lambda: collections.defaultdict(int))
+
+with open(args.acs_name, 'r') as acs_file:
+    rows = csv.DictReader(acs_file)
+    
+    for row in rows:
+        geoid, _ = row.pop('geoid'), row.pop('name')
+        populations[geoid]['Population'] += int(row['B01001001'])
+        populations[geoid]['Population, Error'] += int(row['B01001001, Error'])
+
+logging.info('Read population for {} areas.'.format(len(populations)))
+logging.info('Reading vote counts from {}...'.format(args.votecsv_name))
 
 votes = collections.defaultdict(lambda: collections.defaultdict(float))
 column_pattern = re.compile(r'^(DEM|REP)\d+$')
 
-logging.info('Reading vote counts from {}...'.format(args.filename2))
-
-with gzip.open(args.filename2, 'rt') as file2:
+with gzip.open(args.votecsv_name, 'rt') as file2:
     rows = csv.DictReader(file2)
     
     for row in rows:
@@ -27,23 +41,29 @@ with gzip.open(args.filename2, 'rt') as file2:
                 votes[psid][key] += float(value)
 
 logging.info('Read counts for {} areas.'.format(len(votes)))
-logging.info('Reading areas from {}...'.format(args.filename1))
+logging.info('Reading areas from {}...'.format(args.geo_name))
 
-ds = ogr.Open(args.filename1)
+ds = ogr.Open(args.geo_name)
 features_json = list()
 
-for layer in ds:
-    for feature in layer:
-        feature_json = json.loads(feature.ExportToJson())
-        properties = feature_json['properties']
-        properties.update({key: round(value, 1) for (key, value)
-            in votes[properties['psid']].items() if column_pattern.match(key)})
-        features_json.append(json.dumps(feature_json, sort_keys=True))
+for feature in ds.GetLayer('tracts'):
+    feature_json = json.loads(feature.ExportToJson())
+    properties = feature_json['properties']
+    feature_geoid = properties['geoid']
+    properties.update(populations[f'14000US{feature_geoid}'])
+    features_json.append(json.dumps(feature_json, sort_keys=True))
+
+for feature in ds.GetLayer('precincts'):
+    feature_json = json.loads(feature.ExportToJson())
+    properties = feature_json['properties']
+    properties.update({key: round(value, 1) for (key, value)
+        in votes[properties['psid']].items() if column_pattern.match(key)})
+    features_json.append(json.dumps(feature_json, sort_keys=True))
 
 logging.info('Read {} areas.'.format(len(features_json)))
-logging.info('Writing areas to {}...'.format(args.filename3))
+logging.info('Writing areas to {}...'.format(args.out_name))
 
-with open(args.filename3, 'w') as file3:
+with open(args.out_name, 'w') as file3:
     print('{"type": "FeatureCollection", "features": [', file=file3)
     print(',\n'.join(features_json), file=file3)
     print(']}', file=file3)
