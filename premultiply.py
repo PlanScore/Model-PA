fcda2b86ab70cb3d6a57b5928e17a00ad32ad46e
Michal Migurski <mike@teczno.com>
2018-02-12 21:22:25 +0000

    Updated U.S. votes with new Joint Plan incumbency model

--- premultiply.py
+++ premultiply.py
@@ -9,11 +9,11 @@ with gzip.open(filename1, 'rt') as file1, gzip.open(filename2, 'rt') as file2:
     
     head1, head2 = next(rows1), next(rows2)
     
-    if head1[:8] != head2[:8] or len(head1) != 1008 or len(head2) != 1008:
+    if head1[:7] != head2[:7] or len(head1) != 1009 or len(head2) != 1008:
         raise Exception()
     
     with gzip.open(filename3, 'wt') as file3:
-        columns = ['cntyname', 'mcdname', 'vtdname', 'name', 'stf', 'psid', 'district']
+        columns = ['cntyname', 'mcdname', 'vtdname', 'name', 'stf', 'psid']
         for i in range(1000):
             columns += [f'DEM{i:03d}', f'REP{i:03d}']
         
@@ -21,8 +21,8 @@ with gzip.open(filename1, 'rt') as file1, gzip.open(filename2, 'rt') as file2:
         out.writerow(columns)
         
         for (row1, row2) in zip(rows1, rows2):
-            row = row1[1:8]
-            propDs = list(map(float, row1[8:]))
+            row = row1[1:7]
+            propDs = list(map(float, row1[9:]))
             turnouts = list(map(float, row2[8:]))
             
             print(' '.join(row),
